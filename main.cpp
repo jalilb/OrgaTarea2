3fb02e84260a5c3a816e2ab089405b0920a38f5f
jalilb <jalilburbara@gmail.com>
2014-10-19 18:16:06 -0600

    Terminado

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@ using namespace std;
 //Nota: Se deben poder agregar varias mascotas en un solo archivo
 void escribir(string nombre_archivo, list<Cumpleanios*>cumpleanios)
 {
-
+    //Abre el archivo de forma binaria en un modo escritura
+    ofstream out(nombre_archivo.c_str());
+    //Ciclo que recorre la lista con un iterador
+    for(list<Cumpleanios*>::iterator i=cumpleanios.begin();i!=cumpleanios.end();i++){
+        //Esctitura de los datos en el archivo
+        out<<(*i)->cumpleaniero
+        <<" "
+        <<(*i)->mes
+        <<" "
+        <<(*i)->dia
+        <<" ";
+    }
+    //cierre del archivo
+    out.close();
 }
 
 //Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
@@ -17,7 +30,19 @@ void escribir(string nombre_archivo, list<Cumpleanios*>cumpleanios)
 list<Cumpleanios*> leer(string nombre_archivo)
 {
     list<Cumpleanios*>cumpleanios;
-
+    //Abre el archivo en un modo lectura
+    ifstream in(nombre_archivo.c_str());
+    //declaracion de variables donde se va a almacenar los registros del archivo
+    string cumpleaniero;
+    int mes;
+    int dia;
+    //ciclo de lectura del archivo enseñado en clase
+    while(in>>cumpleaniero){
+        in>>mes;
+        in>>dia;
+        //Insercion de un cumpleañero en la lista de cumpleañeros
+        cumpleanios.push_back(new Cumpleanios(cumpleaniero,dia,mes));
+    }
     return cumpleanios;
 }
 
